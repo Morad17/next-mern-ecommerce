54c7e3e477afc3d443efffcf2f2dd0bf70f296fc
Morad17 <morad123@hotmail.co.uk>
2023-06-12 23:15:06 +0100

    fixing typeerror

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react"
 import MainLayout from "../components/MainLayout"
 import layout from "../components/layout"
 
 export default function Home() {
+
+  const [productsInfo, setProductsInfo ] = useState('')
+  const [loading, isLoading ] = useState(true)
+
+  useEffect(()=> {
+    if (loading === true){
+      
+      fetch('/api/products')
+      .then(res =>  res.json())
+      .then(json => setProductsInfo(json))
+      isLoading(false)
+    }
+    
+  }, [])
+
+  // const productNames = productsInfo.map(p => p.name)
+  // console.log(productNames);
+
   return <MainLayout title="Next-Commerce">Home</MainLayout>
 }
