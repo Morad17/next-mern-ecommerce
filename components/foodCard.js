ebd2573f2f633d44a7f681d55c954e32fe9f514e
Morad17 <morad123@hotmail.co.uk>
2023-06-18 23:18:42 +0100

    created produxct card

--- components/foodCard.js
+++ components/foodCard.js
@@ -1,24 +1,46 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function productCard({
+export default function foodCard({
     food: {id, name, description, cost, calorie, nutrition100g, allegens, image, 
         foodType, subCategories}
 }) {
 
-
   return (
-    <div className="productCard">
+    <div className="food-card">
         <h2 className="card-title">{name}</h2>
-        {/* <Image className="card-image" /> */}
-        <p className="card-description">{description}</p>
+        <div className="card-image">
+          <Image width={300} height={200} src='/assets/images/pancakes.jpg' className="card-image" alt="food-image" />
+        </div>
+        
+        <p className="card-description bold">{description}</p>
         <div className="card-details">
-            <p className="card-nutrition">
-                {/* {nutrition100g} */}
-            </p>
+            <div className="card-nutrition">
+              <ul className="card-keys">
+                <p className="keys bold">Typical values</p>
+                <p className="keys">carbohydrate:</p>
+                <p className="keys">fat:</p>
+                <p className="keys">protein:</p>
+                <p className="keys">salt:</p>
+                <p className="keys">sugars</p>
+              </ul>
+              <ul className="card-values">
+                <p className="values bold">Per 100g</p>
+                <p className="values">{ nutrition100g.carbohydrate}</p>
+                <p className="values">{ nutrition100g.fat}</p>
+                <p className="values">{ nutrition100g.protein}</p>
+                <p className="values">{ nutrition100g.salt}</p>
+                <p className="values">{ nutrition100g.sugars}</p>
+              </ul>
+            </div>
+            
             {/* <div className="card-allegens">{allegens}</div> */}
         </div>
-        <div className="card-cost">{cost}</div>
+        <div className="card-cost">
+          <p className="bold">Cost</p>
+          <p className="bold">{cost}</p>
+        </div>
+        .buy
     </div>
   )
 }
